dbdff03bf0e5d487696e4b900bcce469242af767
Gabriel B Lisboa <gabrielblisboa1800@gmail.com>
2025-10-22 23:06:53 +0000

    cadastro dos 5 territorios

--- war.c
+++ war.c
@@ -9,3 +9,28 @@ struct Territorio {
     char corExercito[30];
     int numTropas;
 };
+
+int main() {
+    struct Territorio territorios[TOTAL_TERRITORIOS];
+    int i;
+
+    printf("=== Cadastro Inicial dos Territórios ===\n\n");
+
+    // Cadastro dos 5 territórios
+    for (i = 0; i < TOTAL_TERRITORIOS; i++) {
+        printf("Território %d:\n", i + 1);
+
+        printf("Nome do território: ");
+        fgets(territorios[i].nome, 50, stdin);
+        territorios[i].nome[strcspn(territorios[i].nome, "\n")] = 0; // remove o \n
+
+        printf("Cor do exército dominante: ");
+        fgets(territorios[i].corExercito, 30, stdin);
+        territorios[i].corExercito[strcspn(territorios[i].corExercito, "\n")] = 0;
+
+        printf("Número de tropas: ");
+        scanf("%d", &territorios[i].numTropas);
+        getchar(); // limpa o ENTER do buffer
+
+        printf("\n");
+    }
